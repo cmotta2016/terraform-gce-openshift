73ee8e93d11d0592aab40f8879194063ff45e47d
Carlos Motta <carlos.motta@elumini.com.br>
2019-07-01 10:11:44 -0300

    Update bucket

--- main.tf
+++ main.tf
@@ -12,12 +12,64 @@ resource "google_storage_bucket" "temp-image-store" {
   labels = {
     ocp-cluster = "${var.clusterid}"
  }
- depends_on = ["null_resource.create_raw_disk"]
 }
 
 resource "google_storage_bucket_object" "rhel-temp-image" {
   name = "rhel-7.5.tar.gz"
   source = "./rhel-7.5.tar.gz"
   bucket = "${google_storage_bucket.temp-image-store.name}"
- depends_on = ["google_storage_bucket.temp-image-store"]
+  depends_on = ["google_storage_bucket.temp-image-store"]
+}
+
+// Add metadata to all project
+resource "google_compute_project_metadata_item" "ssh-key" {
+  key = "sshKey"
+  value = "jeniffer_jc29:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+/2pELvk0l8wtGbznHFXy/v8ifyGg4eT7cx3D3dcFzPGyLQpZkPI44IfPYTBgjcl1NBtKNmWb9VDkKCsaJ6P/KEv1BqWmuGJplYyVKmS829z25/mbWoc7w1shqm59MZTgwyNEU9Q/f7x6/J8F4GR8zmqi8unuuyZ3MI+btHAgotK/WTvoP/qGUe4Wz+x3ofvyVCrX024ND4NZAq9KeTSmZo6rtbTkiDZ6yQgRrzdrIXt+ogmUzmuAQF8KIWNJOEoqNu0jJr6Qja3EjdoHLMv3U9j5YDrIMiImR3JBRs+dgj6sPsKEZlYhbRx57lBuLpAGDARo1iH1DCn+SBK6DQCb jeniffer_jc29"
+  project = "${var.project}"
+}
+
+// This module deploy Network and Subnetwork
+module "network" {
+  source  = "./modules/network"
+  project = "${var.project}"
+}
+
+// Module to deploy bastion node
+module "bastion_node" {
+  source = "./modules/nodes/bastion_node"
+  project = "${var.project}"
+  zone = "${var.zone}"
+  region = "${var.region}"
+  clusterid = "${var.clusterid}"
+  subnetwork-name = "${module.network.subnetwork-name}"
+}
+
+// Module to deploy master node
+module "master_node" {
+  source = "./modules/nodes/master_node"
+  project = "${var.project}"
+  zone = "${var.zone}"
+  region = "${var.region}"
+  clusterid = "${var.clusterid}"
+  subnetwork-name = "${module.network.subnetwork-name}"
+}
+
+// Module to deploy infra node
+module "infra_node" {
+  source = "./modules/nodes/infra_node"
+  project = "${var.project}"
+  zone = "${var.zone}"
+  region = "${var.region}"
+  clusterid = "${var.clusterid}"
+  subnetwork-name = "${module.network.subnetwork-name}"
+}
+
+// Module to deploy apps node
+module "app_node" {
+  source = "./modules/nodes/app_node"
+  project = "${var.project}"
+  zone = "${var.zone}"
+  region = "${var.region}"
+  clusterid = "${var.clusterid}"
+  subnetwork-name = "${module.network.subnetwork-name}"
 }
